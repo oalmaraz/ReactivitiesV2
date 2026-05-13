cb3c6f6e26d72203f359f45e90b0594f630a1790
oalmaraz <oswaldo_alm@hotmail.com>
2026-05-12 21:23:39 -0600

    End of section 2

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
---
+++ Persistence/Migrations/20260506215319_FixActivityConflict.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixActivityConflict : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activites",
+                table: "Activites");
+
+            migrationBuilder.RenameTable(
+                name: "Activites",
+                newName: "Activities");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities");
+
+            migrationBuilder.RenameTable(
+                name: "Activities",
+                newName: "Activites");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activites",
+                table: "Activites",
+                column: "Id");
+        }
+    }
+}
